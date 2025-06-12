d3911e447ecdace3ac9a9fc155339eab4749cd27
iamnas <iamnas.eth@gmail.com>
2025-06-13 00:10:21 +0530

    chore: initialize server application with TypeScript and Socket.IO setup

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,43 @@
+
+import { Server } from 'socket.io';
+
+class SocketService {
+    private _server: Server;
+
+    constructor() {
+        console.log(`Socket service created`);
+        
+        this._server = new Server();
+    }
+    public eventListener() {
+        console.log('Event listener called');
+
+        this._server.on('connect', (socket) => {
+            console.log('Socket connected', socket.id);
+
+            socket.on('event:message', async({message}:{message:string}) => {
+                console.log('Message received', message);
+                socket.emit('event:message', {message:'Hello'});
+
+            });
+
+            socket.on('disconnect', () => {
+                console.log('Socket disconnected');
+            });
+        });
+    }
+
+    get server() {
+        return this._server;
+    }
+
+
+    // public onConnection(socket: Socket) {
+    //     console.log('Socket connected');
+    //     socket.on('disconnect', () => {
+    //         console.log('Socket disconnected');
+    //     });
+    // }
+}
+
+export default SocketService;
